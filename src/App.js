58e1ee6b048ccee8b7e23a7114d6510506110aeb
shubhamghimire <shubhamghimire1996@gmail.com>
2022-03-10 21:25:44 +0900

    addTo function created to add the user input to the list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [todos, setTodos] = useState(['Do medidation.', 'Go for a run.']);
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) => {
+    //This will called when we clicked the button
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
   return (
     <div>
       <h1>Hello from todo-list app</h1>
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)} />
+        <button type="submit" onClick={addTodo}>Add Todo</button>
+      </form>
+
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
